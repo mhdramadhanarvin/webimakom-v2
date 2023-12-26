cd6329378bdcf007cea5654373c685ffa6aa38f3
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2023-12-27 03:01:53 +0700

    feat : finish homepage with all assets

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,365 @@
+<x-new-app-layout>
+    <!-- HEADER -->
+    <div class="fullscreen full-image">
+        <div class="image">
+            <img src="{{ url('./images/pancabudi.jpg') }}" style="" />
+        </div>
+        <div class="overlay content-center first-content tertienary">
+            <div class="content">
+                <div class="row">
+                    <div class="col-6">
+                        <img class="responsive-img logo-stempel" src="{{ url('./images/logo_imakom.png') }}">
+                        {{-- style="width: 300px; height: 300px; border:1px solid tomato; box-shadow: 0 0 5px 5px rgb(165, 84, 70);" /> --}}
+                    </div>
+                    <div class="col-6">
+                        {{-- <span class="text-4xl">Hai...🖐</span> --}}
+                        <span class="text-4xl ">Selamat datang di</span>
+                        <span class="text-4xl text-violet-500 inline-block">IMAKOM</span>
+                        {{-- <span class="text-3xl text-red-500 inline-block">Ikatan Mahasiswa Komputer</span> --}}
+                        <p class="text-xl">
+                            <span class="text-red-500"> Mahasiswa Komputer (IMAKOM) </span> merupakan organisasi resmi
+                            kemahasiswaan pada tingkat
+                            program studi sebagai eksekutor di Universitas Pembangunan Panca Budi.
+                        </p>
+                        {{-- <a href=""><button
+                                style="background:orange ; padding:15px; border-radius:15px; color:white; font-size:15px;">
+                                sobat imakom</button></a> --}}
+                        {{-- <br> --}}
+                        <a href="#first-content"><button class="scroll-for-more"></button></a>
+                    </div>
+                </div>
+                <div class="responsive-gap"></div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ARTIKEL -->
+    <div class="fullscreen tertienary" id="first-content">
+        <div class="header-clear"></div>
+        <div class="content">
+            <div class="row">
+                <div class="col-6">
+                    <h5>Tentang IMAKOM</h5>
+                    <p style="text-indent: 40px; text-align: justify;">
+                        Ikatan Mahasiswa Imakom Universitas Pembangunan Panca Budi (IMAKOM-UNPAB) bersifat mandiri,
+                        kekeluargaan, adil, aspiratif, artisipatif, representatif, efektif, efisien, beserta transparan.
+                        IMAKOM-UNPAB diselenggarakan berdasarkan prinsip dari, oleh, dan untuk mahasiswa Komputer dengan
+                        memberi peranan serta keleluasaan lebih kepada mahasiswa. IMAKOM-UNPAB disahkan pada Tahun 2000,
+                        dan sekarang sudah memasuki usia yang ke 21 dengan tagline “kreatif dan bersahabat”.
+                    </p>
+                    {{-- <br /> --}}
+
+                    <hr class="split tertienary" />
+
+                    {{-- <br /><br /> --}}
+                </div>
+                <div class="col-6">
+                    <h5>Visi</h5>
+                    <p>1. Ikatan Mahasiswa Imakom Universitas Pembangunan Panca Budi diharapkan dapat menjadi suatu
+                        kesatuan
+                        yang utuh dalam mewujudkan solidaritas dan sinergi.</p>
+                    <hr class="split tertienary" />
+
+                    <p>2. Selain itu Ikatan Mahasiswa Imakom Universitas Pembangunan Panca Budi (IMAKOM-UNPAB) bisa
+                        menjadi
+                        pribadi yang lebih aktif, berkompeten, kreatif,inovatif, dan mandiri.</p>
+                    <hr class="split tertienary" />
+                    <br /><br />
+                    <h5>Misi</h5>
+
+                    <p>1. Menaungi mahasiswa Komputer UNPAB agar dapat menjadi pribadi yang bermafaat bagi masyarakat.
+                    </p>
+                    <hr class="split tertienary" />
+
+                    <p>2. Mempererat relasi antara mahasiswa dengan dosen dan staff untuk mencapai kesejahteraan
+                        sekaligus
+                        dapat membangun keharmonisan sebagai satu keluarga.</p>
+                    <hr class="split tertienary" />
+
+                    <p>3. Mengoptimalkan himpunan sebagai wadah aspirasi mahasiswa Komputer Unpab.</p>
+                    <hr class="split tertienary" />
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- FOTO-FOTO -->
+    {{-- <div class="content-shadow" style="">
+        <div class="row no-padding">
+            <div class="col-3">
+                <a class="ukm-box full-image">
+                    <div class="image">
+                        <img src="images/sasude (2).jpeg" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-3">
+                <a class="ukm-box full-image">
+                    <div class="image">
+                        <img src="images/sasude (5).jpeg" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-3">
+                <a class="ukm-box full-image">
+                    <div class="image">
+                        <img src="images/sasude (10).jpeg" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-3">
+                <a class="ukm-box full-image">
+                    <div class="image">
+                        <img src="images/sasude (7).jpeg" />
+                    </div>
+                </a>
+            </div>
+
+        </div>
+        <div class="row no-padding">
+            <div class="col-3">
+                <a class="ukm-box full-image">
+                    <div class="image">
+                        <img src="images/ibb/3.jpeg" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-3">
+                <a class="ukm-box full-image">
+                    <div class="image">
+                        <img src="images/ibb/ibb1 (1).jpg" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-3">
+                <a class="ukm-box full-image">
+                    <div class="image">
+                        <img src="images/ibb/ibb1 (3).jpg" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-3">
+                <a class="ukm-box full-image">
+                    <div class="image">
+                        <img src="images/ibb/ibb2.jpg" />
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div> --}}
+
+    <!-- vidio singkat -->
+    {{-- <div class="fullscreen tertienary" id="first-content">
+        <div class="header-clear"></div>
+        <div class="content">
+            <div class="row">
+                <div class="col-6" style="margin-top:-60px; ">
+                    <video style="border:5px solid orange; border-radius:15px; box-shadow:0 0 5px 5px;" width="430vh"
+                        height="400px" controls>
+                        <source src="images/VS.mp4" type="video/mp4">
+                    </video>
+                </div>
+                <div class="col-6">
+                    <h1>IMAKOM <span style="background:tomato;">MENGABDI</span> </h1>
+                    <h5 style="font-family:cursive;"> TEMA : "meningkatkan potensi anak dalam bidang IT di era Digital"
+                    </h5>
+                    <h4># SASUDE 🖐</h4>
+                    <p style="margin-top:-10px;">Kegiatan ini berlangsung di salah satu sanggar yang bernama sanggar
+                        anak
+                        sungai deli tepat nya di <u style="color:orange;">Tapian sungai deli Jln.Brigjen katamso
+                            kelurahan
+                            sei mati kecamatan medan maimun</u> </p>
+                    <hr class="split tertienary" />
+                </div>
+            </div>
+        </div>
+    </div> --}}
+
+
+    <!-- vidio singkat end-->
+    <!-- Struktur Kepengurusan -->
+    <div class="text-center mt-20">
+        <h3 class="fw-black">
+            STRUKTUR KEPENGURUSAN IMAKOM
+        </h3>
+        <span class="text-3xl font-semibold block mb-3">
+            TA 2023/2024
+        </span>
+        <div class="inline-flex">
+            <img src="{{ url('./images/logo_imakom.png') }}" width="70" height="70" alt="">
+            <img src="{{ url('./images/logo_pancabudi.png') }}" width="70" height="70" alt="">
+        </div>
+        {{-- <p></p><br /> --}}
+    </div>
+
+    <section>
+        <div class="container">
+            <div class="card">
+                <div class="content">
+                    <div class="imgBx"><img src="{{ url('./images/default.jpg') }}" alt=""></div>
+                    <div class="contentBx">
+                        <h2>Ahmad Fernando<br><span>Ketua Umum</span></h2>
+                    </div>
+                </div>
+
+                <ul class="sci">
+                    <li style="--i:1">
+                        <a href="#">
+                            <i class="fa-brands fa-facebook" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:2">
+                        <a href="#">
+                            <i class="fa-brands fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:3">
+                        <a href="#">
+                            <i class="fa-brands fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <div class="imgBx"><img src="{{ url('./images/default.jpg') }}" alt=""></div>
+                    <div class="contentBx">
+                        <h2>Tway Wandiki Lumban Gaol<br><span>Wakil Ketua Umum</span></h2>
+                    </div>
+                </div>
+
+                <ul class="sci">
+                    <li style="--i:1">
+                        <a href="#">
+                            <i class="fa-brands fa-facebook" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:2">
+                        <a href="#">
+                            <i class="fa-brands fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:3">
+                        <a href="#">
+                            <i class="fa-brands fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="container">
+            <div class="card">
+                <div class="content">
+                    <div class="imgBx"><img src="{{ url('./images/default.jpg') }}" alt=""></div>
+                    <div class="contentBx">
+                        <h2>Diza Salsabila<br><span>Sekretaris Umum</span></h2>
+                    </div>
+                </div>
+
+                <ul class="sci">
+                    <li style="--i:1">
+                        <a href="#">
+                            <i class="fa-brands fa-facebook" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:2">
+                        <a href="#">
+                            <i class="fa-brands fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:3">
+                        <a href="#">
+                            <i class="fa-brands fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <div class="imgBx"><img src="{{ url('./images/default.jpg') }}" alt=""></div>
+                    <div class="contentBx">
+                        <h2>Ayu Nadia Lestari<br><span>Wakil Sekretaris Umum</span></h2>
+                    </div>
+                </div>
+
+                <ul class="sci">
+                    <li style="--i:1">
+                        <a href="#">
+                            <i class="fa-brands fa-facebook" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:2">
+                        <a href="#">
+                            <i class="fa-brands fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:3">
+                        <a href="#">
+                            <i class="fa-brands fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <div class="imgBx"><img src="{{ url('./images/default.jpg') }}" alt=""></div>
+                    <div class="contentBx">
+                        <h2>Sutra Nurani<br><span>Bendahara Umum</span></h2>
+                    </div>
+                </div>
+
+                <ul class="sci">
+                    <li style="--i:1">
+                        <a href="#">
+                            <i class="fa-brands fa-facebook" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:2">
+                        <a href="#">
+                            <i class="fa-brands fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:3">
+                        <a href="#">
+                            <i class="fa-brands fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <div class="imgBx"><img src="{{ url('./images/default.jpg') }}" alt=""></div>
+                    <div class="contentBx">
+                        <h2>Muhammad Rizki<br><span>Wakil Bendahara Umum</span></h2>
+                    </div>
+                </div>
+
+                <ul class="sci">
+                    <li style="--i:1">
+                        <a href="#">
+                            <i class="fa-brands fa-facebook" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:2">
+                        <a href="#">
+                            <i class="fa-brands fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li style="--i:3">
+                        <a href="#">
+                            <i class="fa-brands fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</x-new-app-layout>
---
+++ resources/views/layouts/old/app.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="uscope">
+
+<head>
+    <title>Imakom - UNPAB</title>
+    <link rel="shortcut icon" href="{{ url('./images/logo_imakom.png') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ url('./images/logo_imakom.png') }}" type="image/x-icon" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<html>
+
+<body>
+    {{-- <div class="loading-screen loader-visible" id="loader">
+        <img src="{{ url('./images/logo_imakom.png') }}" />
+    </div> --}}
+
+    <!-- Header -->
+    <nav class="nav-dark">
+        <div class="logo light">
+            {{-- <a href="#"><img src="{{ url('./images/kata_imakom.png') }}" alt="" height="50"></a> --}}
+        </div>
+        <div class="logo dark">
+            <a href="#"><img src="{{ url('./images/logo_imakom.png') }}" style="height: 50px; width: 90px;" /></a>
+
+        </div>
+        <div class="menu">
+            <a class="menu-item" href="/"> BERANDA</a>
+            <a class="menu-item" href="#">PENGURUS</a>
+            <a class="menu-item" href="#">PROGRAM KERJA</a>
+            <a class="menu-item" href="#">AGENDA</a>
+            <a class="menu-item" href="#">GALERY</a>
+        </div>
+
+        <div class="nav-icon4" id="mobile-nav">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </nav>
+    {{ $slot }}
+    <!-- finish struktur -->
+    <div class="fullscreen max-h-dvh full-image">
+        <div class="image">
+            <img src="{{ url('./images/footer.jpeg') }}" />
+        </div>
+        <div class="overlay content-center first-content tertienary max-h-dvh">
+            <div class="content">
+                <div class="row">
+                    <div class="col-6">
+                        <h4 class="block-title">Berkarya Bersama Ikatan Mahasiswa Komputer</h4>
+                        <p class="text-lg mb-3">
+                            Ikuti selalu berita terbaru dari IMAKOM melalui sosial media
+
+                        <br />
+                        <div class="social-box">
+                            <a href="https://www.facebook.com/imakom.unpab" class="text-lg block">
+                                <i class="fa-brands fa-facebook"></i>
+                                <span>Imakom UNPAB</span>
+                            </a>
+                            <a href="https://www.instagram.com/imakom_unpab" class="text-lg block">
+                                <i class="fa-brands fa-instagram"></i>
+                                <span>imakom_unpab</span>
+                            </a>
+                            <a href="mailto:imakom@pancabudi.co.id" class="text-lg block">
+                                <i class="fa-solid fa-envelope"></i>
+                                <span>imakom@pancabudi.co.id</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="responsive-gap"></div> --}}
+
+            </div>
+        </div>
+    </div>
+    <div class="text-center p-3" style="background-color:rgba(51, 46, 77, 0.82);">
+        <span>Copyright &copy; 2023 All Right Reserved. Ikatan Mahasiswa Komputer.</span>
+    </div>
+
+    {{-- <footer>
+        <div style="background-color:rgba(51, 46, 77, 0.82); display: flex; justify-content: center; align-items: center; height: 100px;">
+            <h3 style="font-size:15px;">&copy;Ikatan Mahasiswa Komputer</h3>
+        </div>
+    </footer> --}}
+
+
+    {{-- <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script> --}}
+    {{-- <script src="js/lib/jquery-3.2.1.min.js"></script>
+    <script src="js/lib/a-library.js"></script> --}}
+
+    {{-- <script type="text/javascript">
+        (function() {
+            window['__CF$cv$params'] = {
+                r: '6c1f637b39194a02',
+                m: 'fS07O9W_NK1.uDQ.ovkBn9VpAiaMGqVD4cyYCsQFCfo-1640239425-0-AdfMdx7TnZUxSqAwr4NaXt8ktZgYapkAI5t4lBYPzsWyZ9xS6Vi4/LtJw9ghf3kTXg/VDqZgzquLz5OKq6EdtspXnpChc9dJBRBKViaR76kBCZxWTDCfbmk0njXhUCNJzuUHJxZYXLm3HtnVUxkqRZM=',
+                s: [0x293c9eafd3, 0xd5b655de11],
+            }
+        })();
+    </script> --}}
+</body>
+
+</html>
