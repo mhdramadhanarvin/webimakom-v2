48a2c3a1f15cbe8a198f3cd08371b2b5a7322d90
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2024-01-13 13:12:57 +0700

    fix: change to not use modal filament resource for article

---
+++ app/Filament/Resources/ArticleResource/Pages/CreateArticle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ArticleResource\Pages;
+
+use App\Filament\Resources\ArticleResource;
+// use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateArticle extends CreateRecord
+{
+    protected static string $resource = ArticleResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->id();
+
+        return $data;
+    }
+}
