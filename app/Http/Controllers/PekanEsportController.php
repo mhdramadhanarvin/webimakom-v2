768fc96f491994820817bec115b29966c88bb9eb
unknown <ranggarafandi010703@gmail.com>
2024-01-18 14:51:23 +0700

    feat:ganti ke switch case

--- app/Http/Controllers/PekanEsportController.php
+++ app/Http/Controllers/PekanEsportController.php
@@ -14,22 +14,69 @@ class PekanEsportController extends Controller
 
     public function game($game): View
     {
-        if ($game == 'valorant') {
-            return view ('pekanesport_games/valorant');
-        } else if ($game == 'pubgm') {
-            return view ('pekanesport_games/pubgm');
-        } else if ($game == 'mobilelegends') {
-            return view ('pekanesport_games/mobilelegends');
-        } else if ($game == 'freefire') {
-            return view ('pekanesport_games/freefire');
-        } else if ($game == 'psfootball') {
-            return view ('pekanesport_games/psfootball');
-        } else if ($game == 'magicchess') {
-            return view ('pekanesport_games/magicchess');
-        } else if ($game == 'dynastones') {
-            return view ('pekanesport_games/dynastones');
-        } else {
-            return view ('pekanesport_games/game_not_found');
-        }
+        switch ($game) {
+            case 'valorant':
+                return view('pekanesport_games/valorant');
+                break;
+        
+            case 'pubgm':
+                return view('pekanesport_games/pubgm');
+                break;
+        
+            case 'mobilelegends':
+                return view('pekanesport_games/mobilelegends');
+                break;
+        
+            case 'freefire':
+                return view('pekanesport_games/freefire');
+                break;
+        
+            case 'psfootball':
+                return view('pekanesport_games/psfootball');
+                break;
+        
+            case 'magicchess':
+                return view('pekanesport_games/magicchess');
+                break;
+        
+            case 'dynastones':
+                return view('pekanesport_games/dynastones');
+                break;
+        
+            default:
+                return view('pekanesport_games/game_not_found');
+        }        
+    }
+
+    public function form($form): View
+    {
+        switch ($form) {
+            case 'valorantform':
+                return view('pekanesport_form/valorantform');
+                break;
+        
+            case 'pubgmform':
+                return view('pekanesport_form/pubgmform');
+                break;
+        
+            case 'mobilelegendsform':
+                return view('pekanesport_form/mobilelegendsform');
+                break;
+        
+            case 'freefireform':
+                return view('pekanesport_form/freefireform');
+                break;
+        
+            case 'psfootballform':
+                return view('pekanesport_form/psfootballform');
+                break;
+        
+            case 'magicchessform':
+                return view('pekanesport_form/magicchessform');
+                break;
+        
+            default:
+                return view('pekanesport_games/game_not_found');
+        }        
     }
 }
